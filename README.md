fa09ad8de21762143259649c01ab70811ca0ae91
TheRookieGit <InitialDWX@gmail.com>
2025-01-22 19:52:20 -0800

    struct test.cpp

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 https://makefiletutorial.com
 https://stackoverflow.com/questions/3220277/what-do-the-makefile-symbols-and-mean
+https://google.github.io/googletest/reference/assertions.html
+https://en.cppreference.com/w/cpp/container/vector
--- testsrc/StringUtilsTest.cpp
+++ testsrc/StringUtilsTest.cpp
@@ -4,61 +4,107 @@
 using namespace StringUtils;
 
 TEST(StringUtilsTest, SliceTest){
-    
+    EXPECT_EQ(Slice(), );
+    EXPECT_EQ(Slice(), );
+    EXPECT_EQ(Slice(), );
+    EXPECT_EQ(Slice(), );
+    EXPECT_EQ(Slice(), );
+    EXPECT_EQ(Slice(), );
 }
 
 TEST(StringUtilsTest, Capitalize){
-    
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
+    EXPECT_EQ(Capitalize(), );
 }
 
 TEST(StringUtilsTest, Upper){
-    
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+    EXPECT_EQ(Upper(), );
+
 }
 
 TEST(StringUtilsTest, Lower){
-    
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+    EXPECT_EQ(Lower(), );
+
 }
 
 TEST(StringUtilsTest, LStrip){
-    
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+    EXPECT_EQ(LStrip(), );
+
 }
 
 TEST(StringUtilsTest, RStrip){
-    
+    EXPECT_EQ(RStrip(), );
+    EXPECT_EQ(RStrip(), );
+    EXPECT_EQ(RStrip(), );
+    EXPECT_EQ(RStrip(), );
+    EXPECT_EQ(RStrip(), );
+    EXPECT_EQ(RStrip(), );
+
 }
 
 TEST(StringUtilsTest, Strip){
-    
+    EXPECT_EQ(Strip(), );
+    EXPECT_EQ(Strip(), );
+    EXPECT_EQ(Strip(), );
+    EXPECT_EQ(Strip(), );
+    EXPECT_EQ(Strip(), );
+    EXPECT_EQ(Strip(), );
+
 }
 
 TEST(StringUtilsTest, Center){
-    
+    EXPECT_EQ(Center(), );
 }
 
 TEST(StringUtilsTest, LJust){
-    
+    EXPECT_EQ(LJust(), );
 }
 
 TEST(StringUtilsTest, RJust){
-    
+    EXPECT_EQ(RJust(), );
 }
 
 TEST(StringUtilsTest, Replace){
-    
+    EXPECT_EQ(Replace(), );
 }
 
-TEST(StringUtilsTest, Split){
-    
+TEST(StringUtilsTest, Split){ //std::vector<std::string>
+    EXPECT_EQ(Split(), );
 }
 
-TEST(StringUtilsTest, Join){
-    
+TEST(StringUtilsTest, Join){ //std::vector<std::string>
+    EXPECT_EQ(Join(), );
 }
 
 TEST(StringUtilsTest, ExpandTabs){
-    
+    EXPECT_EQ(ExpandTabs(), );
 }
 
 TEST(StringUtilsTest, EditDistance){
-    
+    EXPECT_EQ(EditDistance(), );
 }
